abcbf56f648a874971fb6c6aa3115758c9fe7727
Amelia Kindall <amelia.kindall@gmail.com>
2019-09-25 20:59:45 -0500

    LOGIN-FEATURE add intellij files to gitignore

---
+++ features/step_definitions/login.rb
@@ -0,0 +1,11 @@
+Given(/^a user on the login page$/) do
+  pending
+end
+
+When(/^the user enters valid credentials and successfully logs in$/) do
+  pending
+end
+
+Then(/^the user is redirected to their user page$/) do
+  pending
+end
